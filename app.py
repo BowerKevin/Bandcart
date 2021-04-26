1369d0ee10418c225a210cf66308acc91f0324c4
BowerKevin <kevinbower123@gmail.com>
2021-04-26 19:35:23 -0400

    Render page when Band table is empty
    Allows the Bands page to render when there is no data in the table.

--- app.py
+++ app.py
@@ -13,23 +13,17 @@ db_connection = db.connect_to_database()
 def root():
     return render_template("main.j2")
 
-# @app.route('/bsg-people')
-# def bsg_people():
-#     query = "SELECT * FROM bsg_people;"
-#     cursor = db.execute_query(db_connection=db_connection, query=query)
-#     results = cursor.fetchall()
-#     return render_template("bsg.j2", bsg_people=results)
-
-@app.route('/bands', methods = ['POST', 'GET'])
+@app.route('/bands', methods = ['POST', 'GET', 'PUT', 'DELETE'])
 def bands():
+    print(request.method)
     if request.method == "POST":
-        print("Hello")
+
         bandName = request.form['bandName']
         numMembers = request.form['numMembers']
         genre = request.form['genre']
 
         if bandName == '':
-            return
+            bandName = None
         if numMembers == '':
             numMembers = None
         if genre == "":
@@ -38,11 +32,19 @@ def bands():
         insertQuery = "INSERT INTO `Bands` (`bandName`, `numMembers`, `genre`) VALUES (%s,%s,%s);"
         insertTuple = (bandName, numMembers, genre)
         insertCursor = db.execute_query(db_connection=db_connection, query=insertQuery, query_params=insertTuple) 
-        print(insertTuple)
+    
+    elif request.method == "PUT":
+        print('we want to modify some data')
+    
+    elif request.method == "DELETE":
+        print('we want to delete some data')
 
+
+    
     query = "SELECT * from Bands;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
+    print(results)
     return render_template("bands.j2", Bands=results)
 
 # Listener
