819ff5788cb72bb1bcc7e0d7008bb8e3d08a0410
BowerKevin <kevinbower123@gmail.com>
2021-04-28 13:28:59 -0400

    Upper case to lowercase in sql

--- database/Bandcart.sql
+++ database/Bandcart.sql
@@ -1,8 +1,8 @@
 -- ####################################
 -- Table structure for 'Bands' table ##
 -- ####################################
-drop table if exists `Bands`;
-create table `Bands` (
+drop table if exists `bands`;
+create table `bands` (
     `bandID` int(11) NOT NULL AUTO_INCREMENT,
     `bandName` varchar(255) NOT NULL UNIQUE,
     `numMembers` int(11),
@@ -10,15 +10,15 @@ create table `Bands` (
     PRIMARY KEY(`bandID`)
 )ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
-INSERT INTO `Bands` (`bandName`, `numMembers`, `genre` ) VALUES (
+INSERT INTO `bands` (`bandName`, `numMembers`, `genre` ) VALUES (
    'Radiohead', 5, 'Progressive Rock / Experimental' 
 );
 
 -- #####################################
 -- Table structure for 'Events' table ##
 -- #####################################
-drop table if exists `Events`;
-create table `Events` (
+drop table if exists `events`;
+create table `events` (
     `eventID` int(11) NOT NULL AUTO_INCREMENT,
     `eventName` varchar(255) NOT NULL UNIQUE,
     `eventDate` datetime NOT NULL,
@@ -28,7 +28,7 @@ create table `Events` (
     PRIMARY KEY(`eventID`)
 )ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
-INSERT INTO `Events` (`eventName`, `eventDate`, `eventType`,
+INSERT INTO `events` (`eventName`, `eventDate`, `eventType`,
    `eventCity`, `eventState` ) VALUES (
    'Forecastle Festival', '2015-11-05 13:43:16', 'tour', 'Louisville', 'KY'
 );
@@ -36,25 +36,25 @@ INSERT INTO `Events` (`eventName`, `eventDate`, `eventType`,
 -- #####################################
 -- Table structure for 'BandsEvents' table ##
 -- #####################################
-drop table if exists `BandsEvents`;
-create table `BandsEvents` (
+drop table if exists `bandsevents`;
+create table `bandsevents` (
     `bandID` int(11),
     `eventID` int(11),
     PRIMARY KEY(`bandID`, `eventID`),
-    CONSTRAINT `bandsevents_ibfk_1` FOREIGN KEY (`bandID`) references `Bands` (`bandID`),
-    CONSTRAINT `bandsevents_ibfk_2` FOREIGN KEY (`eventID`) references `Events` (`eventID`)
+    CONSTRAINT `bandsevents_ibfk_1` FOREIGN KEY (`bandID`) references `bands` (`bandID`),
+    CONSTRAINT `bandsevents_ibfk_2` FOREIGN KEY (`eventID`) references `events` (`eventID`)
 )ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
-INSERT INTO `BandsEvents` (`bandID`, `eventID`) VALUES (
-(SELECT bandID from Bands where bandName = "Radiohead"),
-(SELECT eventID from Events where eventName = 'Forecastle Festival')
+INSERT INTO `bandsevents` (`bandID`, `eventID`) VALUES (
+(SELECT bandID from bands where bandName = "Radiohead"),
+(SELECT eventID from events where eventName = 'Forecastle Festival')
 );
 
 -- ########################################
 -- Table structure for 'Customers' table ##
 -- ########################################
-drop table if exists `Customers`;
-create table `Customers` (
+drop table if exists `customers`;
+create table `customers` (
     `customerID` int(11) NOT NULL AUTO_INCREMENT,
     `customerFirst` varchar(255) NOT NULL,
     `customerLast` varchar(255) NOT NULL,
@@ -65,15 +65,15 @@ create table `Customers` (
     constraint full_name unique (`customerFirst`,`customerLast`,`customerDoB`)
 )ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
-INSERT INTO `Customers` (`customerFirst`, `customerLast`, `customerDoB`, `phoneNum`, `email`) 
+INSERT INTO `customers` (`customerFirst`, `customerLast`, `customerDoB`, `phoneNum`, `email`) 
 VALUES ('Kevin', 'Santi', '1996-03-11', '123-456-7890', 'fakemail@123.com'
 );
 
 -- ########################################
 -- Table structure for 'Tickets' table ####
 -- ########################################
-drop table if exists `Tickets`;
-create table `Events` (
+drop table if exists `tickets`;
+create table `tickets` (
     `ticketID` int(11) NOT NULL AUTO_INCREMENT,
     `orderDate` date NOT NULL,
     `price` float(20,2) NOT NULL,
@@ -81,16 +81,16 @@ create table `Events` (
     `customerID` int(11),
     `eventID` int(11),
     PRIMARY KEY(`ticketID`),
-    CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`customerID`) references `Customers` (`customerID`) 
+    CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`customerID`) references `customers` (`customerID`) 
         ON DELETE SET NULL 
         ON UPDATE CASCADE,
-    CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`eventID`) references `Events` (`eventID`) 
+    CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`eventID`) references `events` (`eventID`) 
         ON DELETE SET NULL 
         ON UPDATE CASCADE
 )ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
-INSERT INTO `Tickets` (`orderDate`, `price`, `numTickets`, `customerID`, `eventID`) VALUES (
+INSERT INTO `tickets` (`orderDate`, `price`, `numTickets`, `customerID`, `eventID`) VALUES (
 '2021-04-27', '300.85', '5',
-(SELECT customerID from Customers where customerFirst = "Kevin" and customerLast ="Santi" and customerDoB="1996-03-11"),
-(SELECT eventID from Events where eventName = 'Forecastle Festival')
+(SELECT customerID from customers where customerFirst = "Kevin" and customerLast ="Santi" and customerDoB="1996-03-11"),
+(SELECT eventID from events where eventName = 'Forecastle Festival')
 );
